679b1f6ddc7645f7601885ff6172b2209f184abc
Benjamin Bouillet <YC4116@engie.com>
2020-12-20 12:48:16 +0100

    Mise en place de la home page

---
+++ src/components/Acceuil.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="backgroundPicture">
+    <div class="description">
+      <div class="catch-phrase">Travaillons ensemble pour demain</div>
+      <div class="name">Barbara Bouillet</div>
+      <p class="titre">Assistante dentaire indépendante.</p>
+      <p class="prestations">
+        Acompagnatrice à l'hygiène bucco-dentaire (tout public). Aide à
+        l'évasion et à la détente au soin des enfants. Artisanat en pose de
+        facettes lumineers. Assistanat en 4 ou 6 mains en implantologie,
+        chirurgie ou tout type de soin.
+      </p>
+      <button type="button" class="btn btn-outline-primary">Prestations</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Main",
+};
+</script>
+<style lang="scss" scoped>
+#backgroundPicture {
+  position: relative;
+  min-height: 100vh;
+  background: url("../assets/main-bab.png") no-repeat top;
+  background-size: cover;
+
+  .description {
+    position: absolute;
+    top: 50%; /* poussé de la moitié de hauteur du référent */
+    transform: translateY(-50%); /* tiré de la moitié de sa propre hauteur */
+    width: 50%;
+    margin-left: 10%;
+
+    .catch-phrase {
+      text-transform: uppercase;
+      font-size: 1em;
+      letter-spacing: 2px;
+      margin: 0;
+      font-weight: 100;
+    }
+
+    .titre {
+      font-size: 1.2em;
+      font-weight: bold;
+      color: #cba528;
+    }
+
+    .name {
+      font-size: 3.6em;
+      line-height: 1.1em;
+      color: #2d598d;
+      margin: 4px 0 10px 0;
+    }
+
+    .prestations {
+      text-align: justify;
+      text-justify: inter-word;
+      font-size: 0.9em;
+      line-height: 2em;
+      font-weight: lighter;
+    }
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,104 @@
+<template>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-light">
+    <div class="container">
+      <router-link class="navbar-brand" to="/">
+        <img id="logo" alt="Babnana logo" src="@/assets/logo.jpg" />
+        <span id="header-name">Barbara Bouillet</span>
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navigationContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navigationContent">
+        <ul class="navbar-nav mx-auto mt-2 mt-lg-0">
+          <router-link
+            to="/"
+            class="nav-link"
+            v-slot="{ href, navigate, isExactActive }"
+          >
+            <li :class="{ 'nav-item': true, active: isExactActive }">
+              <a :href="href" class="unstyled-link" @click="navigate"
+                >Acceuil</a
+              >
+            </li>
+          </router-link>
+          <router-link
+            to="/services"
+            class="nav-link"
+            v-slot="{ href, navigate, isExactActive }"
+          >
+            <li :class="{ 'nav-item': true, active: isExactActive }">
+              <a :href="href" class="unstyled-link" @click="navigate"
+                >Prestations</a
+              >
+            </li>
+          </router-link>
+          <router-link
+            to="/tarifs"
+            class="nav-link"
+            v-slot="{ href, navigate, isExactActive }"
+          >
+            <li :class="{ 'nav-item': true, active: isExactActive }">
+              <a :href="href" class="unstyled-link" @click="navigate">Tarifs</a>
+            </li>
+          </router-link>
+          <router-link
+            to="/about"
+            class="nav-link"
+            v-slot="{ href, navigate, isExactActive }"
+          >
+            <li :class="{ 'nav-item': true, active: isExactActive }">
+              <a :href="href" class="unstyled-link" @click="navigate"
+                >A propos</a
+              >
+            </li>
+          </router-link>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+#logo {
+  height: 5em;
+}
+
+.navbar {
+  background-color: white;
+
+  .nav-link {
+    font-size: 1.5em;
+  }
+
+  .nav-item.active {
+    border-top: 5px solid #f6da73;
+  }
+
+  .nav-item {
+    border-top: 5px solid white;
+    &:hover {
+      border-top: 5px solid #f6da73;
+    }
+  }
+}
+
+.unstyled-link,
+.unstyled-link:hover {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
